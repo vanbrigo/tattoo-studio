1fd6c91995ca4ce57f9ebea11bb613f2924ec9bd
Vanessa Brito <vanbrigo@Air-de-Vanessa.home>
2023-10-26 22:54:25 +0200

    feat: add login endpoint to users

--- src/views/usersRoutes.ts
+++ src/views/usersRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { register } from "../controllers/usersControllers";
+import { login, register } from "../controllers/usersControllers";
 
 const router = Router()
 
-router.post('/register', register)
-router.post('/login', )
+router.post('/register',register)
+router.post('/login',login)
 router.post('/profile', )
 router.get('/profile', )
 router.get('/portfolio',)
